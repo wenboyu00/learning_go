5256dcba56298cfe08012cfc9657e7b18f377c20
wenboyu00 <wenboyu00@gmail.com>
2020-09-28 13:42:03 +0800

    添加：concurrent

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -1,6 +1,8 @@
 package engine
 
-import "fmt"
+import (
+	"log"
+)
 
 type ConcurrentEngine struct {
 	Scheduler   Scheduler
@@ -26,7 +28,7 @@ func (e *ConcurrentEngine) Run(seeds ...Request) {
 	for {
 		result := <-out
 		for _, item := range result.Items {
-			fmt.Printf("Got item: %v", item)
+			log.Printf("Got item: %v", item)
 		}
 		for _, request := range result.Requests {
 			e.Scheduler.Submit(request)
--- crawler/main.go
+++ crawler/main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	e := engine.ConcurrentEngine{
-		Scheduler: &scheduler.SimpleScheduler{},
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 10,
 	}
 	e.Run(engine.Request{
 		Url:       "http://www.zhenai.com/zhenghun",
