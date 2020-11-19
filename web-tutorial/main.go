6c2d594eb74f0f9bd58298090b104428e1690aff
wenboyu00 <wenboyu00@gmail.com>
2020-11-19 18:17:27 +0800

    更新：上传文件

--- web-tutorial/main.go
+++ web-tutorial/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -15,11 +16,27 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("welcome"))
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(1024)
+	// [0]第一个文件
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		// ioutil.ReadAll把文件读取成[]byte
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			// string(data)把 []byte 字节切片转换为字符串，写如到 w responseWriter
+			fmt.Fprintln(w, string(data))
+		}
+	}
+}
+
 func main() {
 	server := http.Server{Addr: "localhost:8080"}
 	http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		fmt.Fprintln(w, r.PostFormValue("first_name"))
 	})
+	http.HandleFunc("/upload", upload)
 	server.ListenAndServe()
 }
